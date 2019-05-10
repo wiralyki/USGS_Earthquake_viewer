b390915bec24ca80d38c3a300a877a20dd7c1bf5
amauryval <amauryval@gmail.com>
2019-05-11 00:45:34 +0200

    upd (main.py) simplified

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 # display USA earthquakes
 
-import geojson
-
-
-from shapely.geometry import shape
-
-import geopandas as gdf
-import pandas as df
-
 from geopandas import GeoDataFrame
 from geopandas import GeoSeries
 
@@ -28,27 +20,25 @@ from bokeh.models import Slider
 from bokeh.plotting import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.tile_providers import STAMEN_TERRAIN_RETINA
-
 from bokeh.models.widgets import TextInput
 from core.data_helper import ImportUsgsEarthquakeData
+from bokeh.models import BoxSelectTool
 
 
-class GridsToBokeh(object):
+class ShakeMeToBokeh:
 
     _TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
     def __init__(self):
 
-        source_data_formated = dict(mag=[], place=[], url=[], magType=[], type=[], title=[], x=[], y=[])
-
+        source_data_formated = dict(mag=[], place=[], year=[], url=[], magType=[], type=[], title=[], x=[], y=[])
         self.source_data = ColumnDataSource(data=source_data_formated)
 
-        self._BOKEH_EPSG = "3857"
-
-        self._X_RANGE_DEFAULT = (-200000, 600000)
-        self._Y_RANGE_DEFAULT = (-100000, 700000)
+        self._X_RANGE_DEFAULT = (-800000, 1200000)
+        self._Y_RANGE_DEFAULT = (-400000, 1400000)
 
     def run(self):
+        self._symbology()
         self._bokeh_widgets()
         self._bokeh_map_init()
         self._bokeh_layers_init()
@@ -56,6 +46,13 @@ class GridsToBokeh(object):
 
 
     def _bokeh_map_init(self):
+        hover = HoverTool(tooltips=[
+            ("nom", "@title"),
+            ("Lieu", "@place"),
+            ("Annee", "@year"),
+            ("magnitude", "@mag"),
+            # ("impact", "@detail")
+        ])
 
         # set canvas
         self._plot = figure(
@@ -67,7 +64,7 @@ class GridsToBokeh(object):
             x_axis_type="mercator",
             y_axis_type="mercator",
             output_backend="webgl",
-            tools=self._TOOLS
+            tools=[hover, self._TOOLS]
         )
 
         # set background map
@@ -78,9 +75,9 @@ class GridsToBokeh(object):
         layout = column(
             row(self._plot),
             # row(self._geom_input_wkt, self._from_epsg_value),
-            # row(widgetbox(self._grid_type)),
+            row(self._display_year_widget),
             # row(self._res_x_value, self._res_y_value),
-            # row(self._slider_rotation),
+            row(self._slider),
             # row(widgetbox(self._exporting, self._to_epsg_value))
         )
 
@@ -94,30 +91,51 @@ class GridsToBokeh(object):
             x='x',
             y='y',
             source=self.source_data,
-            size=2,
-            color={'field': 'mag', 'transform': color_mapper},
+            size='mag',
+            color={'field': 'mag', 'transform': self._color_mapper},
             alpha=1,
             legend="earthquake"
         )
-        label = Label(x=1.1, y=18, text=str(years[0]), text_font_size='70pt', text_color='#eeeeee')
-        self._plot.add_layout(label)
 
 
     def _bokeh_widgets(self):
 
-        self._display_year_widget = TextInput(value="500", title="year")
+        self._display_year_widget = TextInput(value="1950", title="Année")
         self._display_year_widget.on_change('value', self.__pull_data_from_year)
+
+        self._slider = Slider(start=1950, end=2019, value=1950, step=1, title="Année")
+        self._slider.on_change('value', self._slider_update)
 
     def __pull_data_from_year(self, attrname, old, new):
         data = ImportUsgsEarthquakeData(
-            self._display_year_widget,
-            self._display_year_widget + 1
+            int(self._display_year_widget.value),
+            int(self._display_year_widget.value) + 1
         ).run()
-
+        print(len(data))
         self.source_data.data = dict(
             mag=data['mag'].tolist(),
             place=data['place'].tolist(),
             url=data['url'].tolist(),
+            year=data['year'].tolist(),
+            magType=data['magType'].tolist(),
+            type=data['type'].tolist(),
+            title=data['title'].tolist(),
+            x=data['x'].tolist(),
+            y=data['y'].tolist()
+        )
+
+    def _slider_update(self, attrname, old, new):
+        data = ImportUsgsEarthquakeData(
+            int(self._slider.value),
+            int(self._slider.value) + 1
+        ).run()
+        print(len(data))
+        # label.text = self._display_year_widget.value
+        self.source_data.data = dict(
+            mag=data['mag'].tolist(),
+            place=data['place'].tolist(),
+            url=data['url'].tolist(),
+            year=data['year'].tolist(),
             magType=data['magType'].tolist(),
             type=data['type'].tolist(),
             title=data['title'].tolist(),
@@ -126,3 +144,13 @@ class GridsToBokeh(object):
         )
 
 
+
+    def _symbology(self):
+        self._color_mapper = LinearColorMapper(
+            palette='Magma256',
+            low=1,
+            high=10
+        )
+
+
+ShakeMeToBokeh().run()
