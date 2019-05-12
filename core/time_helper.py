47cfee08f2c8e540de784567b53991c26fe7bb26
amauryval <amauryval@gmail.com>
2019-05-12 11:21:42 +0200

    fix (time_helper.py) duplicate dates

--- core/time_helper.py
+++ core/time_helper.py
@@ -15,7 +15,7 @@ def get_month_start_end_date_from_period(start_year, end_year):
     dates_list = []
     current_year = start_year
 
-    while current_year <= end_year:
+    while current_year < end_year:
         dates_list.extend([
             get_month_start_end_date(current_year, month_nb)
             for month_nb in range(1, 13)
