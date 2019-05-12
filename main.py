052b13b27fdad714b40b258ddf0ac0f101d5be91
amauryval <amauryval@gmail.com>
2019-05-12 11:58:08 +0200

    upd (main.py) remove widgets text box and add text box

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-# display USA earthquakes
-
-from geopandas import GeoDataFrame
-from geopandas import GeoSeries
-
 from bokeh.io import curdoc
-from bokeh.io import output_file
 from bokeh.layouts import column
-from bokeh.layouts import layout
 from bokeh.layouts import row
-from bokeh.layouts import widgetbox
-from bokeh.models import Button
-from bokeh.models import ColumnDataSource
-from bokeh.models import CustomJS
-from bokeh.models import GeoJSONDataSource
 from bokeh.models import HoverTool
-from bokeh.models import Label
 from bokeh.models import LinearColorMapper
 from bokeh.models import Slider
-from bokeh.plotting import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.tile_providers import STAMEN_TERRAIN_RETINA
 from bokeh.models.widgets import TextInput
 from core.data_helper import ImportUsgsEarthquakeData
-from bokeh.models import ColumnDataSource, TableColumn, DateFormatter, DataTable, NumberFormatter
-from bokeh.models import BoxSelectTool
+from bokeh.models import ColumnDataSource, TableColumn, DataTable, NumberFormatter
+from bokeh.models.widgets import PreText
 
 
 class ShakeMeToBokeh:
-
+    _SOURCES_FILE = 'sources.txt'
     _TOOLS = "box_select,pan,wheel_zoom,box_zoom,reset,save"
 
     def __init__(self):
@@ -52,7 +38,6 @@ class ShakeMeToBokeh:
             ("Lieu", "@place"),
             ("Annee", "@year"),
             ("magnitude", "@mag"),
-            # ("impact", "@detail")
         ])
 
         # set canvas
@@ -71,17 +56,15 @@ class ShakeMeToBokeh:
         # set background map
         self._plot.add_tile(STAMEN_TERRAIN_RETINA)
 
-
     def _bokeh_map_layout(self):
         layout = column(
             row(self._plot),
-            row(self._display_year_widget, self._slider),
+            row(self._slider, self._sources_text),
             row(self._data_table),
         )
 
         curdoc().add_root(layout)
         curdoc().title = "USGS Earthquakes Viewer"
-
 
     def _bokeh_layers_init(self):
 
@@ -107,19 +90,14 @@ class ShakeMeToBokeh:
 
     def _bokeh_widgets(self):
 
-        self._display_year_widget = TextInput(value="1950", title="Choisir une année :")
-        self._display_year_widget.on_change('value', self.__pull_data_from_year)
-
         self._slider = Slider(start=1950, end=2019, value=1950, step=1, title="Barre temporelle")
         self._slider.on_change('value', self._slider_update)
 
-    def __pull_data_from_year(self, attrname, old, new):
-        data = ImportUsgsEarthquakeData(
-            int(self._display_year_widget.value),
-            int(self._display_year_widget.value) + 1
-        ).run()
-        print(len(data))
-        self.source_data.data = self.__format_source_data(data)
+        self._sources_text = PreText(
+            text=open(self._SOURCES_FILE, 'r').read(),
+            width=500,
+            height=100
+        )
 
     def _slider_update(self, attrname, old, new):
         data = ImportUsgsEarthquakeData(
@@ -131,6 +109,11 @@ class ShakeMeToBokeh:
         self.source_data.data = self.__format_source_data(data)
 
     def __format_source_data(self, source_data):
+        """
+
+        :param source_data: geopandas.geodataframe.GeoDataFrame
+        :return: dict
+        """
         return dict(
             mag=source_data['mag'].tolist(),
             place=source_data['place'].tolist(),
